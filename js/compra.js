ee6ade4d57d1237cc94474a34c30339e7f65cd53
Jenyfer Cerrillo <jenyfercerrilloc@gmail.com>
2023-07-02 02:03:29 -0500

    actualizado con fetch

--- js/compra.js
+++ js/compra.js
@@ -6,6 +6,9 @@ const contenedorCarritoProductos = document.querySelector("#carrito_productos");
 const contenedorCarritoFunciones = document.querySelector("#carrito_funciones");
 const contenedorCarritoComprado = document.querySelector("#carrito_comprado");
 const contenedorCarritoDescuento = document.querySelector("#descuento");
+const contenedorCarritoCorreo = document.querySelector("#correo");
+
+
 let botonEliminar = document.querySelectorAll(".carrito_producto_eliminar");
 const botonVaciar = document.querySelector(".carrito_funciones_vaciar");
 const contenedorTotal = document.querySelector("#total");
@@ -14,6 +17,9 @@ const botonComprar = document.querySelector(".carrito_funciones_comprar");
 const inputCodigoDescuento = document.querySelector("#codigo_descuento");
 const btnAplicarDescuento = document.querySelector("#btn_aplicar_descuento");
 const mensajeDescuentoAplicado = document.querySelector("#mensaje_descuento_aplicado");
+
+const btnConsultarCorreo = document.querySelector("#btn_consultar_correo");
+const mensajeElement = document.querySelector("#mensaje_correo");
 
 function cargarProductosCarrito(){
     if (productosCarrito && productosCarrito.length > 0){
@@ -61,6 +67,7 @@ function cargarProductosCarrito(){
         contenedorCarritoFunciones.classList.add("disabled");
         contenedorCarritoComprado.classList.add("disabled");
         contenedorCarritoDescuento.classList.add("disabled");
+        contenedorCarritoCorreo.classList.add("disabled");
     }
 
     actualizarBotonEliminar();
@@ -104,19 +111,58 @@ function actualizarTotal(){
 btnAplicarDescuento.addEventListener("click", aplicarDescuento);
 
 function aplicarDescuento() {
-  const codigoDescuento = inputCodigoDescuento.value;
-  const totalCalculado = productosCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
+    const codigoDescuento = inputCodigoDescuento.value;
+    const totalCalculado = productosCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
   
-  if (codigoDescuento === "nuevoHL") {
-    const descuento = totalCalculado * 0.1; // 10% de descuento
-    const totalConDescuento = totalCalculado - descuento;
-    total.innerHTML = `S/. ${totalConDescuento}`;
-    mensajeDescuentoAplicado.style.display = "inline"; // Mostrar mensaje de descuento aplicado
-  } else {
-    mensajeDescuentoAplicado.style.display = "none"; // Ocultar mensaje de descuento aplicado
+    if (codigoDescuento === "NUEVOHL") {
+      const descuento = totalCalculado * 0.15; // 15% de descuento
+      const totalConDescuento = totalCalculado - descuento;
+      total.innerHTML = `S/. ${totalConDescuento}`;
+      mensajeDescuentoAplicado.style.display = "inline"; // Mostrar mensaje de descuento aplicado
+    } else if (codigoDescuento === "OTRAVEZ23") {
+      const descuento = totalCalculado * 0.2; // 20% de descuento
+      const totalConDescuento = totalCalculado - descuento;
+      total.innerHTML = `S/. ${totalConDescuento}`;
+      mensajeDescuentoAplicado.style.display = "inline"; // Mostrar mensaje de descuento aplicado
+    } else {
+      total.innerHTML = `S/. ${totalCalculado}`;
+      mensajeDescuentoAplicado.style.display = "none"; // Ocultar mensaje de descuento aplicado
+    }
   }
-}
 
+
+// Agregar evento de envío del formulario
+
+
+btnConsultarCorreo.addEventListener("click", function () {
+    const correo = document.querySelector("#codigo_correo").value;
+
+    // Expresión regular para validar el formato de correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(correo)) {
+        // Si el correo no cumple el formato, mostrar un mensaje de error
+        mensajeElement.style.display = "block";
+        mensajeElement.textContent = "Por favor, ingresa una dirección de correo electrónico válida.";
+        return; // Detener la ejecución del código
+    }
+
+    fetch("../js/usuarios.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const usuario = data.find((user) => user.email === correo);
+            if (usuario) {
+                mensajeElement.style.display = "block";
+                mensajeElement.textContent = "Bienvenido de nuevo, te regalamos un 20% de descuento. Ingresa el código OTRAVEZ23";
+            } else {
+                mensajeElement.style.display = "block";
+                mensajeElement.textContent = "Bienvenido, por ser nuevo usuario te regalamos el 15% de descuento. Ingresa el código NUEVOHL";
+            }
+        })
+        .catch((error) => {
+            console.log("Error al obtener los usuarios:", error);
+        });
+});
 
 
 botonComprar.addEventListener("click", comprarCarrito);
@@ -129,7 +175,5 @@ function comprarCarrito(){
     contenedorCarritoFunciones.classList.add("disabled");
     contenedorCarritoComprado.classList.remove("disabled");
     contenedorCarritoDescuento.classList.add("disabled");
+    contenedorCarritoCorreo.classList.add("disabled");
 }
-
-
-
